5ed61c4ea1f343a99786426c6ab6a64ee90c74e6
cororing <corock.dev@gmail.com>
2022-04-09 19:48:24 +0900

    실 객체를 통한 IllegalArgumentException 테스트 코드 작성

---
+++ src/test/java/com/nhnacademy/pms/tdd/EntranceNewTest.java
@@ -0,0 +1,25 @@
+package com.nhnacademy.pms.tdd;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class EntranceNewTest {
+    private Entrance entrance;
+
+    @BeforeEach
+    void setUp() {
+        entrance = new Entrance();
+    }
+
+    @DisplayName("[1] 번호판을 인식할 수 없는 차가 주차장에 들어오면 예외를 발생한다.")
+    @Test
+    void scan_unknownLicensePlate_forACar() {
+        Car car = new Car(null);
+
+        assertThatIllegalArgumentException().isThrownBy(() -> entrance.scanLicensePlateNumber(car))
+            .withMessageContaining("null");
+    }
+}
